8631fd61520e234cd1d72c1c595bcb85377d1004
MiAneko <mariya.serova2010@yandex.ru>
2024-01-08 01:03:20 +0300

    wanna cry a bit (not a bit)

--- v2/common/build.gradle.kts
+++ v2/common/build.gradle.kts
@@ -2,9 +2,10 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm")
+    kotlin("kapt") version "1.9.20"
 }
 
-group = "com.mianeko"
+group = "com.mianeko.common"
 version = "0.0.1-SNAPSHOT"
 
 java.sourceCompatibility = JavaVersion.VERSION_17
@@ -15,4 +16,16 @@ tasks.withType<KotlinCompile> {
         jvmTarget = "17"
         languageVersion = "1.9"
     }
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict", "-Xcontext-receivers")
+        jvmTarget = "17"
+        languageVersion = "1.9"
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
 }
--- v2/gateway/build.gradle.kts
+++ v2/gateway/build.gradle.kts
@@ -29,13 +29,11 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.mockito:mockito-core")
-	implementation("org.ktorm:ktorm-core:$ktormVersion")
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.1.0")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
 	implementation("org.springframework.boot:spring-boot-starter-websocket")
-	implementation("org.springframework.boot:spring-boot-starter-jdbc")
 
 	implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
 }
